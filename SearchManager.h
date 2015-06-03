4379eff4383bcdb8334e39853ecf006a958d8d04
DestinyCookie <jmldwyer@gmail.com>
2015-06-02 23:56:06 -0700

    search_manager_barebones

---
+++ SearchManager.h
@@ -0,0 +1,12 @@
+#ifndef SEARCHMANAGER_H
+#define SEARCHMANAGER_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void searchManager();
+void caseSearch();
+void caseList();
+
+#endif
