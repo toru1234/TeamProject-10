fd224a3221ff26566698940fa0946995c456d4a8
toru1234 <toru1234@users.noreply.github.com>
2015-06-03 01:45:56 -0700

    Create BinaryNode.h

---
+++ BinaryNode.h
@@ -0,0 +1,34 @@
+
+#ifndef __TeamProject__BinaryNode__
+#define __TeamProject__BinaryNode__
+
+#include <stdio.h>
+#include "Website.h"
+
+class BinaryNode
+{
+private:
+    Website              item;         // Data portion
+    BinaryNode* leftPtr;		// Pointer to left child
+    BinaryNode* rightPtr;		// Pointer to right child
+    
+public:
+    // constructors
+    BinaryNode(const Website & web)	{item = web; leftPtr = 0; rightPtr = 0;}
+    BinaryNode(const Website & web,
+               BinaryNode* left,
+               BinaryNode* right) {item = web; leftPtr = left; rightPtr = right;}
+    // accessors
+    void setItem(const Website & web) {item = web;}
+    void setLeftPtr(BinaryNode* left) {leftPtr = left;}
+    void setRightPtr(BinaryNode* right) {rightPtr = right;}
+    // mutators
+    Website getWebsite() const	 {return item;}
+    BinaryNode* getLeftPtr() const  {return leftPtr;}
+    BinaryNode* getRightPtr() const {return rightPtr;}
+    
+    bool isLeaf() const {return (leftPtr == 0 && rightPtr == 0);}
+}; 
+
+
+#endif
