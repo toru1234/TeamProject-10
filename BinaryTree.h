71cd2f0ce1fea7b19873d908df92f74110deaff4
toru1234 <toru1234@users.noreply.github.com>
2015-06-03 01:44:30 -0700

    Create BinaryTree.h

---
+++ BinaryTree.h
@@ -0,0 +1,64 @@
+
+#ifndef __TeamProject__BinaryTree__
+#define __TeamProject__BinaryTree__
+
+#include <stdio.h>
+#include "BinaryNode.h"
+#include <iostream>
+
+class BinaryTree
+{
+protected:
+    BinaryNode* rootPtr;		// ptr to root node
+    int count;							// number of nodes in tree
+    
+public:
+    // "admin" functions
+    BinaryTree() {rootPtr = 0; count = 0;}
+    // copy constructor
+    BinaryTree(const BinaryTree & tree);
+    virtual ~BinaryTree() { }
+    BinaryTree & operator = (const BinaryTree & sourceTree);
+    
+    // common functions for all binary trees
+    bool isEmpty() const	{return count == 0;}
+    int size() const	    {return count;}
+    void clear()			{destroyTree(rootPtr); rootPtr = 0; count = 0;}
+    void preOrder(void visit(Website &)) const {_preorder(visit, rootPtr);}
+    void inOrder(void visit(Website &)) const  {_inorder(visit, rootPtr);}
+    void inOrder(void visit(Website &),
+                 const Website & minItem,
+                 const Website & maxItem) const;
+    void postOrder(void visit(Website &)) const{_postorder(visit, rootPtr);}
+    void bfsOrder(void visit(Website &)) const;
+    void indented() const {_printTreeIndented(rootPtr, 1);}
+    bool searchInRange(const Website & minVal, const Website & maxVal);
+    
+    
+    
+    // abstract functions to be implemented by derived class
+    virtual bool insert(const Website & newData) = 0;
+    virtual bool remove(const Website & data) = 0;
+    virtual bool getEntry(const Website & anEntry, Website & returnedItem) const = 0;
+    BinaryNode* searchInRange(const Website & minRange, const Website & maxRange) const;
+    
+private:
+    // delete all nodes from the tree
+    void destroyTree(BinaryNode* nodePtr);
+    
+    // copy from the tree rooted at nodePtr and returns a pointer to the copy
+    BinaryNode* copyTree(const BinaryNode* nodePtr);
+    
+    // internal traverse
+    void _preorder(void visit(Website &), BinaryNode* nodePtr) const;
+    void _inorder(void visit(Website &), BinaryNode* nodePtr) const;
+    void _inorder(void visit(Website &),
+                  BinaryNode* nodePtr,
+                  const Website & minItem,
+                  const Website & maxItem) const;
+    void _postorder(void visit(Website &), BinaryNode* nodePtr) const;
+    void _printTreeIndented(BinaryNode* nodePtr, int depth) const;
+    
+}; 
+
+#endif
