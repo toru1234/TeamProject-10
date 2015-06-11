9c75a099e2785e34c4b26df5109b65de4608ee72
toru1234 <toru1234@users.noreply.github.com>
2015-06-11 01:07:08 -0700

    Update Hash_Table.h

--- Hash_Table.h
+++ Hash_Table.h
@@ -1,8 +1,8 @@
 /*******************************************
-Hash Table class
-a hash table class that used buckets for
-collision resolution.
-*******************************************/
+ Hash Table class
+ a hash table class that used buckets for
+ collision resolution.
+ *******************************************/
 #ifndef HASH_TABLE_H_INCLUDED
 #define HASH_TABLE_H_INCLUDED
 
@@ -14,17 +14,15 @@ using namespace std;
 class Hash_Table
 {
 private:
-    //record the size of the table
-    int tablesize;
-    //record the size of the bucket
-    int bucketsize;
-    //record the number of collisions
-    int collisions;
-    //record the number of loaded buckets
-    int load;
-    //record the number of full buckets
-    int fullBuckets;
+    int tablesize;  //record the size of the table
+    int bucketsize;  //record the size of the bucket
+   
+    int collisions;  //record the number of collisions
+    
+    int load;   //record the number of loaded buckets
+    int fullBuckets;    //record the number of full buckets
 
+    
     struct Hash_Entry
     {
         //the number of element that has filled the bucket
@@ -34,9 +32,9 @@ private:
     };
     //the hash table array
     Hash_Entry *table;
-
+    
     int getKey(const string &input) const;
-
+    
 public:
     //constructor
     Hash_Table();
@@ -44,17 +42,20 @@ public:
     Hash_Table(const int &tableSize, const int &bucketSize);
     //destructor
     ~Hash_Table();
-
+    
+    //get the pointer of hash_entry
+    struct Hash_Entry* getHash_Entry() const{ return table;}
     //get the pointer of data corresponding the key
     Website* getData(const string &strkey) const;
     // getters for the hash size and the table size
     int getTableSize() const {return tablesize;}
     int getBucketSize() const {return bucketsize;}
-
+    
     bool insert(const Website &input);
     void PrintTable() const;
     void displayList() const;
     void showStatistics() const;
     Website* getHashItem(int tableIndex, int bucketIndex) const;
 };
+
 #endif // HASH_TABLE_H_INCLUDED
