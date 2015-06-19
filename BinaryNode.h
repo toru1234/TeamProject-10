966375984da6dd3696721ae0b383070db845bfd6
toru1234 <toru1234@users.noreply.github.com>
2015-06-18 21:37:20 -0700

    Update BinaryNode.h

--- BinaryNode.h
+++ BinaryNode.h
@@ -8,26 +8,26 @@ class BinaryNode
 {
 private:
     string key;      // key to search, add, etc
-    Website item;         // Data portion
+    Website* item;         // Data portion
     BinaryNode* leftPtr;		// Pointer to left child
     BinaryNode* rightPtr;		// Pointer to right child
     
 public:
     // constructors
-    BinaryNode(const Website & web)	{item = web; leftPtr = 0; rightPtr = 0;}
-    BinaryNode(const Website & web,
+    BinaryNode(Website* web)	{item = web; leftPtr = 0; rightPtr = 0;}
+    BinaryNode(Website* web,
                BinaryNode* left,
                BinaryNode* right) {item = web; leftPtr = left; rightPtr = right;}
     
     // accessors
     void setKey(const string inputKey) {key = inputKey;}
-    void setItem(const Website & web) {item = web;}
+    void setItem(Website* web) {item = web;}
     void setLeftPtr(BinaryNode* left) {leftPtr = left;}
     void setRightPtr(BinaryNode* right) {rightPtr = right;}
     
     // mutators
     string getKey() const {return key;}
-    Website getWebsite() const	 {return item;}
+    Website* getWebsite() const	 {return item;}
     BinaryNode* getLeftPtr() const  {return leftPtr;}
     BinaryNode* getRightPtr() const {return rightPtr;}
     
